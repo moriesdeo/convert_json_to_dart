6c0f82d9644dfe70eab6334e5284a52195c6d4b8
MORIES DEO HUTAPEA <dheocm@ymail.com>
2024-08-11 13:16:05 +0700

    initial commit

--- lib/json_to_kotlin.dart
+++ lib/json_to_kotlin.dart
@@ -7,52 +7,7 @@ String jsonToKotlin(String className, Map<String, dynamic> json) {
     final type = _getKotlinType(value, capitalize(_convertToCamelCase(key)));
     buffer.writeln('    val ${_convertToCamelCase(key)}: $type,');
   });
-  buffer.writeln(') {');
-  buffer.writeln();
-
-  // Factory method for JSON deserialization
-  buffer.writeln('    companion object {');
-  buffer.writeln('        fun fromJson(json: Map<String, Any?>): $className {');
-  buffer.writeln('            return $className(');
-  json.forEach((key, value) {
-    final type = _getKotlinType(value, capitalize(_convertToCamelCase(key)));
-    if (type.startsWith('List<')) {
-      final itemType = type.substring(5, type.length - 1);
-      if (_isPrimitiveType(itemType)) {
-        buffer
-            .writeln('                ${_convertToCamelCase(key)} = (json["$key"] as List<*>).map { it as $itemType }.toList(),');
-      } else {
-        buffer.writeln(
-            '                ${_convertToCamelCase(key)} = (json["$key"] as List<*>).map { $itemType.fromJson(it as Map<String, Any?>) }.toList(),');
-      }
-    } else if (!_isPrimitiveType(type)) {
-      buffer.writeln('                ${_convertToCamelCase(key)} = $type.fromJson(json["$key"] as Map<String, Any?>),');
-    } else {
-      buffer.writeln('                ${_convertToCamelCase(key)} = json["$key"] as $type,');
-    }
-  });
-  buffer.writeln('            )');
-  buffer.writeln('        }');
-  buffer.writeln('    }');
-  buffer.writeln();
-
-  // Method for JSON serialization
-  buffer.writeln('    fun toJson(): Map<String, Any?> {');
-  buffer.writeln('        return mapOf(');
-  json.forEach((key, value) {
-    final type = _getKotlinType(value, capitalize(_convertToCamelCase(key)));
-    if (type.startsWith('List<')) {
-      buffer.writeln('            "$key" to ${_convertToCamelCase(key)}.map { it.toJson() },');
-    } else if (!_isPrimitiveType(type)) {
-      buffer.writeln('            "$key" to ${_convertToCamelCase(key)}.toJson(),');
-    } else {
-      buffer.writeln('            "$key" to ${_convertToCamelCase(key)},');
-    }
-  });
-  buffer.writeln('        )');
-  buffer.writeln('    }');
-  buffer.writeln('}');
-  buffer.writeln();
+  buffer.writeln(')');
 
   // Recursive generation of nested classes
   json.forEach((key, value) {
@@ -60,11 +15,11 @@ String jsonToKotlin(String className, Map<String, dynamic> json) {
     if (type.startsWith('List<')) {
       final itemType = type.substring(5, type.length - 1);
       if (value is List && value.isNotEmpty && value.first is Map) {
-        buffer.writeln(jsonToKotlin(itemType, (value.first as Map).cast<String, dynamic>()));
+        buffer.writeln('\n' + jsonToKotlin(itemType, (value.first as Map).cast<String, dynamic>()));
       }
     } else if (!_isPrimitiveType(type)) {
       if (value is Map) {
-        buffer.writeln(jsonToKotlin(type, value.cast<String, dynamic>()));
+        buffer.writeln('\n' + jsonToKotlin(type, value.cast<String, dynamic>()));
       }
     }
   });
