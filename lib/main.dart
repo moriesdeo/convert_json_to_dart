d05eaca82fb6810aef622ac5d0ce487415bf974f
moriesdeo <moriesdeo242@gmail.com>
2025-08-12 23:32:20 +0700

    Removed `window_manager` initialization and platform-specific logic from `main.dart` to simplify the app entry point.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:window_manager/window_manager.dart';
 
 import 'my_app.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    await windowManager.ensureInitialized();
-    await windowManager.setMinimumSize(const Size(600, 400));
-    await windowManager.setMaximizable(true);
-  }
-
+void main() {
   runApp(const MyApp());
 }
